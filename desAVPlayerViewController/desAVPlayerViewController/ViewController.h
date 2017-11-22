e1a2630d81a78752701c977b38edaf3a2f0a526b
desunire <desunire@163.com>
2017-11-22 15:58:52 +0800

    初始化项目文档

---
+++ desAVPlayerViewController/desAVPlayerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  desAVPlayerViewController
+//
+//  Created by desunire on 2017/11/22.
+//  Copyright © 2017年 desunire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
