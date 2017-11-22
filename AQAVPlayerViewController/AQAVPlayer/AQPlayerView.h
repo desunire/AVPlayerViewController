1e502b9d2beecc553ccea12709a4ee336da40f89
desunire <desunire@163.com>
2017-11-22 17:07:03 +0800

    init

---
+++ AQAVPlayerViewController/AQAVPlayer/AQPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  AQPlayerView.h
+//  desAVPlayerViewController
+//
+//  Created by desunire on 2017/11/22.
+//  Copyright © 2017年 desunire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AQPlayerView : UIView
+
+@end
